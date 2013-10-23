d78f246553dd8263e14c98aa363be122d39b0a62
HarmNo13 <No13@sport.com.ua>
2013-10-23 11:36:27 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,27 @@
-test git
-test git
+#test git
+#!/usr/bin/python
+# program of calculator
+import os
+from Tkinter import *
+
+root=Tk()
+
+
+def num1(event):		# (num0...num9)
+	global s
+	s=s+'1'
+	os.system('clear')
+	print(s)
+
+def num2(event):
+	global s
+	s=s+'2'	
+	os.system('clear')
+	print(s)
+
+def num3(event):
+	global s
+	s=s+'3'	
+	os.system('clear')
+	print(s)
+
